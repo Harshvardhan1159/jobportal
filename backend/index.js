61adc4adcf7f6488fa506378a2033b6018ff846a
Harshvardhan1159 <124015582+Harshvardhan1159@users.noreply.github.com>
2024-12-02 18:55:47 +0530

    models
    added different models

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import database from "./utils/database/databse.js";
+dotenv.config({});
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -15,6 +18,7 @@ app.use(cors(corsOptions));
 
 
 app.listen(PORT,()=>{
+    database();
     console.log(`Server running at port ${PORT}`);
     
 })
